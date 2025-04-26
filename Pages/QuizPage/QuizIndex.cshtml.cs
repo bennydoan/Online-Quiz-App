4279da3c01e66136645aa9e96c6cb817ae1daf34
Luke Doan <bennydoan1111@gmail.com>
2025-04-26 19:55:14 +0930

    Finsih the Quiz Collection page

--- Pages/QuizPage/QuizIndex.cshtml.cs
+++ Pages/QuizPage/QuizIndex.cshtml.cs
@@ -2,25 +2,26 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Quiz_Application2.Data;
 using Quiz_Application2.Models;
-using Quiz_Application2.Pages.QuizPage;
 
-namespace Quiz_Application2.Pages
+namespace Quiz_Application2.Pages.QuizPage
 {
-    public class IndexModel : PageModel
+    public class QuizIndexModel : PageModel
     {
         private readonly AppDbContext _context;
 
-        public IndexModel(AppDbContext context) // Dependcy injection
+        public QuizIndexModel(AppDbContext context)
         {
             _context = context;
         }
 
         public IEnumerable<Quiz> Quizzes { get; set; } = new List<Quiz>(); // Initialize the list to avoid null reference exceptions
 
-
+       
         public void OnGet()
         {
+
             Quizzes = _context.Quizzes.ToList(); // Fetch all quizzes from the database
+
         }
     }
 }
